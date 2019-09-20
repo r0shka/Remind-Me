91949bce7e07e7c32943ce8e0a837b8a147c9d1a
Vadym <roshkav2@gmail.com>
2019-09-20 23:23:28 +0200

    entity edit

--- app/src/main/java/com/example/fragmentviewmodel/db/entity/NotificationTask.java
+++ app/src/main/java/com/example/fragmentviewmodel/db/entity/NotificationTask.java
@@ -1,4 +1,41 @@
 package com.example.fragmentviewmodel.db.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notification_task_table")
 public class NotificationTask {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
+    private Integer id;
+
+    @ColumnInfo(name = "title")
+    @NonNull
+    private String title;
+
+    @ColumnInfo(name = "description")
+    private String description;
+
+
+    public NotificationTask(@NonNull String title, String description){
+        this.title = title;
+        this.description = description;
+    }
+
+    @NonNull
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public int getId(){
+        return this.id;
+    }
 }
