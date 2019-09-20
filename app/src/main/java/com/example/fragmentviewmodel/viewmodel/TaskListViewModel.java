f304572c90114f0e820d399cb431e70e4dc96512
Vadym <roshkav2@gmail.com>
2019-09-21 00:34:20 +0200

    create list ViewModel

--- app/src/main/java/com/example/fragmentviewmodel/viewmodel/TaskListViewModel.java
+++ app/src/main/java/com/example/fragmentviewmodel/viewmodel/TaskListViewModel.java
@@ -1,7 +1,28 @@
 package com.example.fragmentviewmodel.viewmodel;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class TaskListViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.fragmentviewmodel.TaskRepository;
+import com.example.fragmentviewmodel.db.entity.NotificationTask;
+
+import java.util.List;
+
+public class TaskListViewModel extends AndroidViewModel {
+
+    private TaskRepository repository;
+    private LiveData<List<NotificationTask>> allTasks;
+
+    public TaskListViewModel(@NonNull Application application) {
+        super(application);
+        repository = new TaskRepository(application);
+        allTasks = repository.getAllTasks();
+    }
+
+    public LiveData<List<NotificationTask>> getAllTasks(){
+        return this.allTasks;
+    }
 }
